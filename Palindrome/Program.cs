3b27f401de4d3aaab59ba2822222b42b2b122d62
Martin <rauch83@yandex.ru>
2022-08-17 00:15:48 +0300

    Palindrome adde

---
+++ Palindrome/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("введите число из пяти цифр");
+int a = Convert.ToInt32(Console.ReadLine());
+string str = a.ToString();
+int[] b = new int[str.Length];
+for( int i=0; i< str.Length; i++)
+{
+    b[i] = int.Parse(str[i].ToString());
+}
+
+
+if (b.Length == 5)
+    if(b[0] == b[4])
+        {
+            if(b[1] == b[3])
+            Console.Write("Число "  + (str) + " палиндром.");         
+        }
+    else
+        {
+            Console.Write("Число "  + (str) + " не палиндром .");
+        }
+else
+if (b.Length > 5)
+{
+    Console.Write("Ошибка! Вы ввели больше пяти цифр.");
+}
+if (b.Length < 5)
+{
+    Console.Write("Ошибка!Вы ввели меньше пяти цифр!");
+}
+
+
+
+// if (b.Length == 5)
+// {
+//     Console.Write("Третья цифра числа - ");
+//     PrintArray(b);
+//     Console.WriteLine();
+// }
