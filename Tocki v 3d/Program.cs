c250045c6460dd75cbcb700bab6b2de321641075
Martin <rauch83@yandex.ru>
2022-08-18 00:17:26 +0300

    Tocki v 3d added

---
+++ Tocki v 3d/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите координаты точки А");
+Console.WriteLine("xA =");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("yA =");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("zA =");
+int zA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B");
+Console.WriteLine("xB =");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("yB =");
+int yB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("zB =");
+int zB  = Convert.ToInt32(Console.ReadLine());
+
+int sAB = (xA - xB)*(xA - xB)+(yA - yB)*(yA - yB)+(zA - zB)*(zA - zB);
+
+double a = Math.Sqrt(sAB);
+
+Console.WriteLine(a);
